73808dde3cf07e988949cfd96221d2041db15843
niauniau <cshtheresa@gmail.com>
2021-07-24 12:27:23 +0800

    模組 practiceLeetCode 的初始簽入

---
+++ practiceLeetCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practiceLeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
